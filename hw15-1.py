8b61052504331b5dee5e1cedff2e6bca67cd8613
Alex Joliet <p21ajoliet@fairfieldprep.org>
2021-02-04 21:23:01 -0500

    began hw

---
+++ hw15-1.py
@@ -0,0 +1,33 @@
+#Author: ALJ (AMDG) 2/3/21
+
+
+
+#Question 1
+fruits = ('banana', 'orange', 'grape', 'peach')
+
+#Question 2
+print(fruits[2])
+
+#Question 3
+print(fruits[1][0])
+
+#Question 4
+print(len(fruits))
+
+#Question 5
+print('apple' not in fruits)
+
+#Question 6
+print(list(fruits))
+
+#Question 7
+print((4, 5, 6) + (1, 2, 3))
+
+#Question 8
+print(('a') * 5)
+
+#Question 9
+
+
+#Question 10
+del(fruits)
